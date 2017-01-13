a01917c7bc16aaf93b933b6e7414d9205d600055
tirzaelise <tirza.soute@student.uva.nl>
2017-01-13 14:30:33 +0100

    started working on navigation drawer

--- Meet-Eat/app/src/main/java/nl/mprog/meeteat/MainActivity.java
+++ Meet-Eat/app/src/main/java/nl/mprog/meeteat/MainActivity.java
@@ -3,11 +3,19 @@ package nl.mprog.meeteat;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private GoogleApiClient mGoogleApiClient;
+    private ListView drawerList;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +56,74 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient
         signInButton.setOnClickListener(this);
         findViewById(R.id.joiningButton).setOnClickListener(this);
         findViewById(R.id.cookingButton).setOnClickListener(this);
+
+        drawerList = (ListView) findViewById(R.id.navigationList);
+        createDrawerItems();
+        setDrawerListener();
+        setUpDrawer();
+    }
+
+    /** Creates menu items for the navigation drawer */
+    private void createDrawerItems() {
+        String[] menuArray = {"Home", "Account"};
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                menuArray);
+        drawerList.setAdapter(adapter);
+    }
+
+    /** Sets a listener on the navigation drawer */
+    private void setDrawerListener() {
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /** Sets up the navigation drawer */
+    private void setUpDrawer() {
+        final ActionBar actionBar = getSupportActionBar();
+        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.openDrawer,
+                R.string.closeDrawer) {
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                if (actionBar != null) setTitle("Navigation");
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                if (actionBar != null) actionBar.setTitle(getString(R.string.app_name));
+                invalidateOptionsMenu();
+            }
+        };
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 
     /** Attaches listener to FirebaseAuth instance */
